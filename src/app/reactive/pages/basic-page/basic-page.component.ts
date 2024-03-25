c350cf948f65db4668ac75e1672cab1140b6fade
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-25 18:41:38 -0500

    Add reactive form to interact with 'basic-page'

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``
 })
 export class BasicPageComponent {
+
+  public myForm: FormGroup = this.formBuilder.group({
+    name:       [''],
+    price:      [0],
+    inStorage:  [0]
+  })
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
+
+  onSave(): void {
+    console.log(this.myForm.value);
+  }
 
 }
