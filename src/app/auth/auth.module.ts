7858211d259f5853ceca4573ffba6b85427c17e8
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-25 12:59:26 -0500

    Create components pages for the project.
    Add HTML structure for the component pages, the structure was taken from 'https://gist.github.com/Klerith/5729dcfb170595f84d1b564a182ffbc2'.
    The components pages don't have selector.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,9 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegisterPageComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
