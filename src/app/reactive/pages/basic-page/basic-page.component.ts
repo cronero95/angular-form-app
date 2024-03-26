75e4a93a518d0bb4db7e5ceee3089de2f009f3f6
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-25 21:50:19 -0500

    Show form properties and use form reset().

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,23 +1,35 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const xboxSeriesX = {
+  name: 'Xbox Series X',
+  price: 3200000,
+  inStorage: 17
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
 
   public myForm: FormGroup = this.formBuilder.group({
-    name:       [''],
-    price:      [0],
-    inStorage:  [0]
+    name:       ['', [Validators.required, Validators.minLength(3)]],
+    price:      [0, [Validators.required, Validators.min(0)]],
+    inStorage:  [0, [Validators.required, Validators.min(0)]]
   })
 
   constructor(
     private formBuilder: FormBuilder
   ) {}
 
+  ngOnInit(): void {
+    this.myForm.reset(xboxSeriesX);
+  }
+
   onSave(): void {
+    if(this.myForm.invalid) return;
+
     console.log(this.myForm.value);
   }
 
