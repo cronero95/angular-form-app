1c504f6a695907eeb18942ada2c6d2b1f0d7dd39
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-26 22:41:18 -0500

    Create an asynchronous validator.
    This validator is just for test, it doesn't have validation logic, just return the same observable.
    When using asynchronous validators the form status can be: valid, invalid, or pending (while the asynchronous validation is processed).

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ValidatorsService } from '../../../shared/services/validators.service';
+import { EmailValidatorService } from '../../../shared/validators/email-validator.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -10,7 +11,7 @@ export class RegisterPageComponent {
 
   public myForm: FormGroup = this.formBuilder.group({
     name: ['', [Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern)]],
-    email: ['', [Validators.required, Validators.pattern(this.validatorsService.emailPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorsService.emailPattern)], [this.emailValidator]],
     username: ['', [Validators.required, this.validatorsService.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]]
@@ -18,7 +19,8 @@ export class RegisterPageComponent {
 
   constructor(
     private formBuilder: FormBuilder,
-    private validatorsService: ValidatorsService
+    private validatorsService: ValidatorsService,
+    private emailValidator: EmailValidatorService
   ) {}
 
   isValidField(field: string): (boolean | null) {
