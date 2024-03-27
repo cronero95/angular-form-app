716d8f66278b1f7dd90ae361c6bb6decad458e44
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-26 20:59:20 -0500

    Connect HTML form and reactive form

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
   styles: ``
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]]
+  })
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
+
+  isValidField(field: string) {
+    //TODO: Get validation from a service
+  }
+
+  onSubmit(): void {
+    this.myForm.markAllAsTouched();
+  }
 
 }
